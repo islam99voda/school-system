af5f20c18ae40640e403e331a7d726800d7487b7
islam ahmed <islam.voda99@gmail.com>
2023-10-08 17:21:38 +0300

    CRUD Teacher using interface

--- app/Http/Controllers/Teachers/TeacherController.php
+++ app/Http/Controllers/Teachers/TeacherController.php
@@ -2,74 +2,56 @@
 
 namespace App\Http\Controllers\Teachers;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreTeachers;
+use App\Repository\TeacherRepositoryInterface;
 
 class TeacherController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
+    protected $Teacher;
+
+    public function __construct(TeacherRepositoryInterface $Teacher)
+    {
+        $this->Teacher = $Teacher;
+    }
+
+    
     public function index()
     {
-        echo "done";
+        $Teachers = $this->Teacher->getAllTeachers();
+        return view('pages.Teachers.Teachers',compact('Teachers'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function create()
     {
-        //
+        $specializations = $this->Teacher->Getspecialization();
+        $genders = $this->Teacher->GetGender();
+        return view('pages.Teachers.create',compact('specializations','genders'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+     
+    public function store(StoreTeachers $request)
     {
-        //
+      return $this->Teacher->StoreTeachers($request);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    
+    
     public function edit($id)
     {
-        //
+        $Teachers = $this->Teacher->editTeachers($id);
+        $specializations = $this->Teacher->Getspecialization();
+        $genders = $this->Teacher->GetGender();
+        return view('pages.Teachers.edit',compact('Teachers','specializations','genders'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
+    
+    public function update(Request $request)
     {
-        //
+        return $this->Teacher->UpdateTeachers($request);
     }
 
     /**
@@ -78,8 +60,8 @@ class TeacherController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Request $request)
     {
-        //
+        return $this->Teacher->DeleteTeachers($request);
     }
 }
--- app/Http/Models/Teacher.php
+++ app/Http/Models/Teacher.php
@@ -2,10 +2,32 @@
 
 namespace App\Http\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Translatable\HasTranslations;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Teacher extends Model
 {
-    use HasFactory;
+    use HasTranslations;
+    public $translatable = ['Name'];
+    protected $guarded=[];
+
+    // علاقة بين المعلمين والتخصصات لجلب اسم التخصص
+    public function specializations()
+    {
+        return $this->belongsTo('App\Http\Models\Specialization', 'Specialization_id');
+    }
+
+    // علاقة بين المعلمين والانواع لجلب جنس المعلم
+    public function genders()
+    {
+        return $this->belongsTo('App\Http\Models\Gender', 'Gender_id');
+    }
+
+// علاقة المعلمين مع الاقسام
+    public function Sections()
+    {
+        return $this->belongsToMany('App\Http\Models\Section','teacher_section');
+    }
 }
+
