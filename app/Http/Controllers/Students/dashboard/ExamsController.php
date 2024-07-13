9cae1e50fdd2289e1109a1b676f296f38195450a
islam ahmed <islam99voda>
2024-07-13 23:07:39 +0300

    create exams controller

---
+++ app/Http/Controllers/Students/dashboard/ExamsController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Students\dashboard;
+
+use App\Http\Models\Quizze;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ExamsController extends Controller
+{
+
+    public function index()
+    {
+        $quizzes = Quizze::where('grade_id', auth()->user()->Grade_id)
+            ->where('classroom_id', auth()->user()->Classroom_id)
+            ->where('section_id', auth()->user()->section_id)
+            ->orderBy('id', 'DESC')
+            ->get();
+        return view('pages.Students.dashboard.exams.index', compact('quizzes'));
+    }
+
+    public function show($quizze_id)
+    {
+        return $quizze_id;
+    }
+
+}
